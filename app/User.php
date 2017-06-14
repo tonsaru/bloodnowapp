ba41b5bf93f8cc0c8eace4b3836fb4b7afce5f9d
SURASARU\Saru <surachat.work@gamil.com>
2017-06-14 17:24:55 +0700

    init

---
+++ app/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'email', 'password' , 'blood' , 'phone' , 'province' , 'birthyear' , 'countdonate' , 'user_img'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
